ea1c74ef684bfc8c203d675e1c90679e3cfa26ab
lypinator <t_lyp@yahoo.com>
2023-05-12 13:46:03 -0700

    remove built files

--- Examples/MAX78000/Hello_World/build/startup_max78000.d
+++
@@ -1,2 +0,0 @@
-/home/analog-person/MaximSDK_RPI/Examples/MAX78000/Hello_World/build/startup_max78000.o: \
- /home/analog-person/MaximSDK_RPI/Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
